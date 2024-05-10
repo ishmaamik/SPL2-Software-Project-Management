2714c3c56c1813dd2347a669fa287644e57e05ad
ishmaam-ik <ishmaam@iut-dhaka.edu>
2024-05-10 20:55:00 +0600

    Added credentials

---
+++ StudentDialog.jsx
@@ -0,0 +1,75 @@
+import {Dialog, Box, TextField, Button} from '@mui/material';
+import { useContext, useState} from 'react';
+import SignUpDialog from '../Credentials/SignUpDialog';
+import { authenticateStudentLogin } from '../../service/api';
+import { DataContext } from '../../context/DataProvider';
+import { ErrorClose, OnValueChange } from '../../CommonFunctionalities';
+import { ButtonWrapper, BoldType, Wrapper, Boxer, dialogStyle, BoxStyle, Errorz} from './CSS-Creds';
+
+
+const LoginInitials=
+{
+    
+    Id: '',
+    Password: ''
+}
+
+const StudentDialog= ({open, SetOpen})=>
+{
+    // const [oven, signupOpen] = useState(false);
+    const [login, setLogin]= useState(LoginInitials);
+    const {setAccount}=useContext(DataContext);
+    const [errorz, setErrorz]= useState(false);
+
+const ErrorClose=()=>
+{
+    setErrorz(false);
+    SetOpen(false);
+}
+
+    const loginUser= async()=>
+   {
+    try{
+    let response= await authenticateStudentLogin(login);
+    if(response.status===200)
+    {
+        ErrorClose(SetOpen, setErrorz);
+        setAccount(response.data.data.StudentId);
+        
+    }
+    else
+    {
+        setErrorz(true);
+    }
+}
+catch(error)
+{
+    console.error('Error:', error);
+    setErrorz(true);
+
+}
+   }
+
+    return(
+        <Dialog open={open} onClose={()=> ErrorClose(SetOpen, setErrorz)} PaperProps={{sx:dialogStyle}} hideBackdrop={false}>
+         <Wrapper>
+
+                <BoldType>Student ID:</BoldType>
+                <TextField variant='standard' onChange={(e) =>OnValueChange(e, login, setLogin)} name='ID' style={{fontSize:60, marginTop:0, marginLeft: 10}} label='Enter ID'/>       
+            </Wrapper>
+            
+            { errorz && <Errorz>Please enter valid Student ID or Password</Errorz> }
+            <Box style={{marginTop:50, marginLeft:70, display:'flex'}}>
+                <BoldType >Password:</BoldType>
+                <TextField variant='standard' onChange={(e)=>OnValueChange(e, login, setLogin)} name='Password' style={{fontSize:60, marginTop:0, marginLeft: 10}} label='Enter Password'/>       
+                
+                </Box> 
+
+                <ButtonWrapper>
+                <Button variant="contained" onClick={loginUser}>Login</Button>
+            </ButtonWrapper>
+                </Dialog>
+    )
+}
+
+export default StudentDialog;
---
+++ SupervisorSignup.jsx
@@ -0,0 +1,66 @@
+import {Dialog, Box, TextField, Button} from "@mui/material";
+import { useState, useContext } from "react";
+import { authenticateSignUp, authenticateSupervisorSignUp } from "../../service/api";
+import {DataContext} from '../../context/DataProvider';
+import { ButtonWrapper, dialogStyle } from "./CSS-Creds";
+
+const SignupInitials=
+{
+    Name: '',
+    Password:'',
+    Department: '',
+    Programme: '',
+    Email: ''
+}
+
+
+
+const StudentSignup= ({open, SetOpen}) =>
+{
+    const onClose=() =>
+{
+    SetOpen(false);
+}
+    
+const [signup, setSignup] =useState(SignupInitials);
+const{setAccount}= useContext(DataContext);
+
+const onInputChange=(e)=>
+    {
+    //    console.log(e.target.value);
+
+     setSignup({...signup, [e.target.name]: e.target.value});
+     console.log(signup);
+    }
+
+const signupUser= async()=>
+{
+    await authenticateSupervisorSignUp(signup);
+    setAccount(signup.StudentId);
+    onClose();
+}
+    return(
+        
+
+        <Box>
+        <Dialog open={open} onClose={()=>onClose()} PaperProps={{sx:dialogStyle}}>
+
+            
+            <TextField variant='standard' onChange={(e)=>onInputChange(e, signup, setSignup)} name='Name' style={{fontSize:60, marginTop:0, marginLeft: 10}} label='Enter Name'/>       
+
+            <TextField variant='standard' onChange={(e)=>onInputChange(e, signup, setSignup)} name='Password' style={{fontSize:10, marginTop:0, marginLeft: 10}} label='Enter Password'/>
+           
+            <TextField variant='standard' onChange={(e)=>onInputChange(e, signup, setSignup)} name='Department' style={{fontSize:10, marginTop:0, marginLeft: 10}} label='Enter Department'/>
+           
+            <TextField variant='standard' onChange={(e)=>onInputChange(e, signup, setSignup)} name='Email' style={{fontSize:10, marginTop:0, marginLeft: 10}} label='Enter Email'/>
+
+            <ButtonWrapper> <Button onClick={() => signupUser()}variant="contained">Continue </Button></ButtonWrapper>
+                
+
+        </Dialog>
+
+        </Box>
+    )
+}
+
+export default StudentSignup;
